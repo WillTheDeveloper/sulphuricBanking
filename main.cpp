01d9bb3bf70494bacf1da49ff6f70587363b2230
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-08-16 11:37:56 +0100

    broke

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <vector>
 
 int main() {
     std::fstream ffs;
@@ -8,7 +8,7 @@ int main() {
 
     std::cout << "Welcome to Sulphuric Banking!" << std::endl;
     std::string name;
-    int account;
+    std::vector<std::string> account;
 
     std::cout << "Enter your full name: ";
     std::cin >> name;
@@ -16,7 +16,18 @@ int main() {
     std::cout << "Enter your account number:" << std::endl;
     std::cin >> account;
 
-    ffs.open(filename, std::ios::in)
+    ffs.open(filename, std::ios::in);
+
+    std::vector<std::string> accountnumber;
+    std::string fullname, dob, number;
+
+    while (ffs >> reinterpret_cast<bool &>(accountnumber)) {
+        if (accountnumber == account) {
+            std::cout << "Welcome " << name << "!" << std::endl;
+            std::cout << "Your account number is " << account << std::endl;
+//            std::cout << "Your balance is $" << balance << std::endl;
+        }
+    }
 
     return 0;
 }
